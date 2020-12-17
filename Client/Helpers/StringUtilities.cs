4a6a63db9d953c4f991ba663d3d7e776c6f5023a
vazhadval <vazhadvalishvili@gmail.com>
2020-12-17 11:38:13 +0400

    initial

---
+++ Client/Helpers/StringUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string GetClassName(int count)
+        {
+            return count >= 0 ? "text-success" : "text-danger";
+        }
+    }
+}
