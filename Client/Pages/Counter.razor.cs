36bd24ae9abce3ffcf85cbdce873b7ef18030daa
vazhadval <vazhadvalishvili@gmail.com>
2020-12-23 14:20:08 +0400

    inputimage

--- Client/Pages/Counter.razor.cs
+++ Client/Pages/Counter.razor.cs
@@ -9,26 +9,10 @@ namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
-        [Inject]
-        public IJSRuntime JS { get; set; }
-        [CascadingParameter]
-        public string Color { get; set; }
-
         private int currentCount = 0;
-        private static int count = 0;
-
-        [JSInvokable]
         public void IncrementCount()
         {
             currentCount++;
-            count++;
-            JS.InvokeVoidAsync("dotnetStaticInvokation");
-        }
-
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(count);
         }
     }
 }
